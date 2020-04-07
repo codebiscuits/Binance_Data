1357dbbe00fe123ca3ebe789fb3eb4bcdbcf43a3
codebiscuits <58611002+codebiscuits@users.noreply.github.com>
2020-04-07 23:27:00 +0100

    Create get_all_info.py

---
+++ get_all_info.py
@@ -0,0 +1,18 @@
+from binance.client import Client
+import keys
+from get_all_pairs import get_pairs
+
+client = Client(api_key=keys.Pkey, api_secret=keys.Skey)
+
+pairs_list = get_pairs('USDT')
+
+x, y = 0, 0
+
+for pair in pairs_list:
+    info = client.get_symbol_info(pair)
+    if info.get('status') == 'TRADING':
+        x += 1
+    elif info.get('status') == 'BREAK':
+        print(info.get('symbol'))
+        y += 1
+print(f'Trading: {x} Break: {y}')
