a719916fe3c6df4a30d567e9100e12a6d7c90c2e
Ross Coates <ross_coates82@hotmail.com>
2020-04-08 10:36:00 +0100

    fixed percentage and time taken calculations on trades.py

--- get_all_trades.py
+++ get_all_trades.py
@@ -25,7 +25,7 @@ for i in range(i_range):
     last_old_trade = all_trades.iloc[-1, 0]
     for j in range(j_range):
         start = time.perf_counter()
-        # print(f'Major loop {j+1} of {j_range}')
+        print(f'Major loop {j+1} of {j_range}')
         if j == 0:
             k_range = request_limit-1
         else:
@@ -34,7 +34,7 @@ for i in range(i_range):
             # if (k+1) % 200 == 0:
                 # print(f'Minor loop: {k + 1}')
             total_loops = j_range * request_limit
-            current_loop = (j+1) * k
+            current_loop = (j*k_range) + k
             r = round((current_loop / total_loops) * 100)
             if r-s:
                 print(f'{r}% completed')
@@ -44,13 +44,13 @@ for i in range(i_range):
             all_trades = all_trades.append(new_trades, ignore_index=True, sort=True)
             last_old_trade = all_trades.iloc[-1, 0]
         end = time.perf_counter()
-        loop_time = end - start
+        loop_time = round(end - start)
         if loop_time < 61:
             time.sleep(61 - loop_time)
         inclusive_end = time.perf_counter()
-        total_loop_time = inclusive_end - start
-        print(all_trades.iloc[-1])
-        print(f'Loop time: {loop_time}, Loop time including sleep: {total_loop_time}')
+        total_loop_time = round(inclusive_end - start)
+        # print(all_trades.iloc[-1])
+        print(f'Loop time: {loop_time//60}m {loop_time%60}s, Including sleep: {total_loop_time//60}m {total_loop_time%60}s')
         print('-' * 40)
     file_path = Path(f'Data/trades/{pair}_{i}.csv')
     all_trades.to_csv(file_path)
